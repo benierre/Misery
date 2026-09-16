f95ee41dff4cca1926025624b4ee676d2687c3b5
benierre <benicioescobar80@gmail.com>
2026-09-16 16:03:31 -0300

    feat: initialize project with package.json and add tanque class with tests
    - Created package.json for project setup with vitest as a dev dependency.
    - Implemented tanque class with methods to handle life and damage.
    - Added unit tests for tanque class to verify its functionality.

---
+++ src/tanque.ts
@@ -0,0 +1,15 @@
+export class tanque {
+   private vida: number;
+
+   constructor() {
+       this.vida = 2;
+   }
+
+    recibirDisparo(): void {
+    this.vida--;
+   }
+
+   estaVivo(): boolean{
+     return this.vida > 0
+   }
+}
---
+++ tests/tanque.test.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest';
+import { tanque } from "../src/tanque";
+
+describe("tanque", () =>{
+ it ("El tanque debe estar vivo", () => {
+    const tanque1 = new tanque
+
+    expect(tanque1.estaVivo()).toBe(true);
+  });
+ it("El tanque debe morir de 2 disparos", () => {
+   const tanque1= new tanque
+   
+   tanque1.recibirDisparo();
+   tanque1.recibirDisparo();
+
+    expect(tanque1.estaVivo()).toBe(false);
+ });
+})
+ 
+
+ 
+
+ 
